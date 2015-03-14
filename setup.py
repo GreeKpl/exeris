013e4bb6d451ef91960a1afa293334ca39cbf853
attomir <attomir@gmail.com>
2015-03-14 18:57:53 +0100

    Add setup.py packaging information

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from os import path
+
+setup(
+    name='exeris',
+    version='0.1',
+
+    description='exeris',
+    long_description='',
+
+    url='https://github.com/GreeKpl',
+
+    author='GreeK',
+    author_email='grkalk@gmail.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='game',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+
+    install_requires=['sqlalchemy'],
+
+    extras_require = {
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    },
+)
