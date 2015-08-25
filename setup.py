8a07d9256bb7357b5bce3614fb42985e0231027e
attomir <attomir@gmail.com>
2015-08-25 18:46:24 +0200

    Add Flask dependencies

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
 
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
 
-    install_requires=['sqlalchemy', 'flask', 'flask-sqlalchemy', 'geoalchemy2', 'psycopg2', 'shapely', 'pillow',
+    install_requires=['sqlalchemy', 'flask', 'flask-bootstrap', 'flask-sijax', 'flask-sqlalchemy', 'flask-security', 'geoalchemy2', 'psycopg2', 'shapely', 'pillow',
                       'wtforms'],
 
     extras_require = {
