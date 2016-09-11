399b89d6894d43e69496c2ab368a2dc56cfa511e
GreeK <grkalk@gmail.com>
2016-09-11 23:08:49 +0200

    [106] Link to dashboard on front page

--- exeris/outer/views.py
+++ exeris/outer/views.py
@@ -1,8 +1,9 @@
 # noinspection PyUnresolvedReferences
 from exeris.outer import outer_bp, socketio_events
 from flask import render_template
+from flask.ext.login import current_user
 
 
 @outer_bp.route("/")
 def front_page():
-    return render_template("front_page.html")
+    return render_template("front_page.html", logged_in=current_user.is_authenticated())
