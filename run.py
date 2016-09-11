028cef0fff6d626e1cfb7a29eb1e403aedc44cf4
GreeK <grkalk@gmail.com>
2016-09-11 17:35:53 +0200

    [106] Use command line args for sync_users.py and run.py

--- run.py
+++ run.py
@@ -1,6 +1,13 @@
+import sys
+
 from exeris.app import app, socketio
 
-if False:
-    socketio.run(app, "0.0.0.0", debug=False)
+if len(sys.argv) > 1:
+    PORT = int(sys.argv[1])
 else:
-    socketio.run(app, "127.0.0.1", debug=True)
+    PORT = 5000
+
+if False:
+    socketio.run(app, "0.0.0.0", port=PORT, debug=False)
+else:
+    socketio.run(app, "127.0.0.1", port=PORT, debug=True)
--- util/sync_users.py
+++ util/sync_users.py
@@ -1,3 +1,5 @@
+import sys
+import importlib.machinery
 import psycopg2
 
 """
@@ -5,10 +7,12 @@ Synchronizes two databases by INSERTing rows missing in destination database fro
 It assumes both databases have the same structure of tables.
 """
 
-import importlib.machinery
+if len(sys.argv) < 3:
+    print("Invalid arguments. Command format: python3 sync_users.py [SOURCE_CONFIG] [DESTINATION_CONFIG]")
+    exit(1)
 
-source_config = importlib.machinery.SourceFileLoader('config', '../exeris/config/default_config.py').load_module()
-destination_config = importlib.machinery.SourceFileLoader('config', '../exeris/config/default_config.py').load_module()
+source_config = importlib.machinery.SourceFileLoader('config', sys.argv[1]).load_module()
+destination_config = importlib.machinery.SourceFileLoader('config', sys.argv[2]).load_module()
 
 conn_source = psycopg2.connect(source_config.SQLALCHEMY_DATABASE_URI)
 conn_destination = psycopg2.connect(destination_config.SQLALCHEMY_DATABASE_URI)
@@ -27,11 +31,14 @@ players_on_source = get_player_ids(cur_source)
 
 missing_players_ids = [player for player in players_on_source if player not in players_on_destination]
 
-cur_source.execute("SELECT * FROM players WHERE id IN %s", (tuple(missing_players_ids),))
-players_to_add = cur_source.fetchall()
+if missing_players_ids:
+    cur_source.execute("SELECT * FROM players WHERE id IN %s", (tuple(missing_players_ids),))
+    players_to_add = cur_source.fetchall()
 
-for player_tuple in players_to_add:
-    cur_destination.execute("INSERT INTO players VALUES %s", (player_tuple,))
-cur_destination.commit()
+    for player_tuple in players_to_add:
+        cur_destination.execute("INSERT INTO players VALUES %s", (player_tuple,))
+    cur_destination.commit()
 
-print("Players:", missing_players_ids, "have been copied from source to destination")
+    print("Players:", missing_players_ids, "have been copied from source to destination")
+else:
+    print("There are no players to copy from source to destination")
