9f23713213b7fec5199e77d0fce01d258b5e0a9d
GreeK <grkalk@gmail.com>
2017-02-12 00:43:59 +0100

    [171] Listen for '_after' events in entity actions

--- front/src/modules/entities-actionsAddon.js
+++ front/src/modules/entities-actionsAddon.js
@@ -1,11 +1,19 @@
 import socket from "../util/server";
-import {requestRefreshEntity, deselectEntity, selectEntityAction, clearSelectedEntityAction} from "./entities";
+import {
+  requestRefreshEntity,
+  deselectEntity,
+  selectEntityAction,
+  clearSelectedEntityAction,
+  requestRootEntities
+} from "./entities";
 
 export const ENTITY_ACTION_TAKE = "ENTITY_ACTION_TAKE";
 export const ENTITY_ACTION_DROP = "ENTITY_ACTION_DROP";
 export const ENTITY_ACTION_GIVE = "ENTITY_ACTION_GIVE";
 export const ENTITY_ACTION_EAT = "ENTITY_ACTION_EAT";
 export const ENTITY_ACTION_PUT_INTO_STORAGE = "ENTITY_ACTION_PUT_INTO_STORAGE";
+export const ENTITY_ACTION_BIND_TO_VEHICLE = "ENTITY_ACTION_BIND_TO_VEHICLE";
+
 
 export const setUpSocketioListeners = dispatch => {
 
@@ -63,18 +71,56 @@ export const setUpSocketioListeners = dispatch => {
     dispatch(requestRefreshEntity(characterId, storageId));
   });
 
-  socket.on("after_unbind_from_vehicle", (entities) => {
-    // for (let i = 0; i < entities.length; i++) {
-    //   $.publish("entities:refresh_entity_info", entities[i]);
-    // }
+  socket.on("character.attack_after", (characterId, entityId) => {
+    dispatch(standardAfterEntityAction(characterId, entityId));
   });
 
-  socket.on("after_start_boarding_ship", (entity_id) => {
-    // $.publish("entities:refresh_entity_info", entity_id);
+  socket.on("character.unbind_from_vehicle_after", (characterId, entityIds) => {
+    for (let entityId of entityIds) {
+      dispatch(standardAfterEntityAction(characterId, entityId));
+    }
   });
 
-  socket.on("after_start_unboarding_from_ship", (entity_id) => {
-    // $.publish("entities:refresh_entity_info", entity_id);
+  socket.on("character.bind_to_vehicle_setup", (characterId, entities) => {
+    dispatch(selectEntityAction(characterId, ENTITY_ACTION_BIND_TO_VEHICLE, entities));
+  });
+
+  socket.on("character.bind_to_vehicle_after", (characterId, entityIds) => {
+    for (let entityId of entityIds) {
+      dispatch(standardAfterEntityAction(characterId, entityId));
+    }
+  });
+
+  socket.on("character.start_boarding_ship_after", (characterId, entityId) => {
+    dispatch(standardAfterEntityAction(characterId, entityId));
+  });
+
+  socket.on("character.start_unboarding_from_ship_after", (characterId, entityId) => {
+    dispatch(standardAfterEntityAction(characterId, entityId));
+  });
+
+  socket.on("character.start_burying_entity_after", (characterId, entityId) => {
+    dispatch(standardAfterEntityAction(characterId, entityId));
+  });
+
+  socket.on("character.start_taming_animal_after", (characterId, entityId) => {
+    dispatch(standardAfterEntityAction(characterId, entityId));
+  });
+
+  socket.on("character.toggle_closeable_after", (characterId, entityId) => {
+    dispatch(standardAfterEntityAction(characterId, entityId));
+  });
+
+  socket.on("character.move_to_location_after", (characterId) => { // enter
+    dispatch(requestRootEntities(characterId));
+  });
+
+  socket.on("character.go_to_location_after", (characterId) => { // travel to
+    console.log("TODO GO TO LOCATION");
+  });
+
+  socket.on("character.show_readable_contents_after", (characterId, readableEntity) => {
+    console.log("TODO READABLE ENTITY");
   });
 };
 
