4cc4de660782704613c70d6b4892be0d71f70dcd
GreeK <grkalk@gmail.com>
2017-05-19 00:23:38 +0200

    [186] Create an endpoint to report missing texts

--- exeris/player/views.py
+++ exeris/player/views.py
@@ -21,6 +21,25 @@ def page_player(path):
     return render_template("front/dist/index.html")
 
 
+@player_bp.route("/report_missing_tag", methods=['POST'])
+def report_missing_tag():
+    """
+    API endpoint that (when run in debug mode) allows to report tag keys missing on the client-side.
+    When not run in the debug mode, then it does nothing with the value.
+    :return: 
+    """
+    if not app.config["DEBUG"]:
+        return ""
+
+    missing_keys_info = dict(request.form)
+    del missing_keys_info["_t"]  # remove date
+    assert len(missing_keys_info) == 1, "Missing tag should give a single key"
+    missing_tag_key = list(missing_keys_info.keys())[0]
+
+    g.pyslate.t(missing_tag_key, client_side=True)  # ask pyslate to call standard "missing key" code
+    return ""
+
+
 @player_bp.route("/discourse_sso")
 def discourse_sso_login():
     if not main.app.config["ENABLE_SSO_FOR_DISCOURSE"]:
