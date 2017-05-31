d9a6ba504d6d49a241dc86027dc2a2c2b20194e5
GreeK <grkalk@gmail.com>
2017-06-01 00:12:06 +0200

    [189] Remove files which were used for old server-side templates

--- exeris/player/static/js/player/page_dashboard.js
+++
@@ -1,9 +0,0 @@
-FRAGMENTS.dashboard = (function($) {
-
-    $(document).on("click", "#create_character", function() {
-        var char_name = $("#char_name").val();
-        socket.emit("create_character", char_name, function() {
-            $.publish("player/character_added");
-        });
-    });
-})(jQuery);
