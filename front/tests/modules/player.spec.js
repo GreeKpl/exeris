cc26a8d8d4ad90528314356b0a6419614ab272d8
GreeK <grkalk@gmail.com>
2017-06-10 00:44:40 +0200

    [197] Test async methods in the 'player' module

--- front/tests/modules/player.spec.js
+++ front/tests/modules/player.spec.js
@@ -1,8 +1,10 @@
 import {
   playerReducer,
-  updateOwnCharactersList, updateAchievementsList,
+  updateOwnCharactersList, updateAchievementsList, requestOwnCharactersList, UPDATE_OWN_CHARACTERS_LIST,
+  createNewCharacter, UPDATE_ACHIEVEMENTS_LIST, requestAchievementsList,
 } from "../../src/modules/player";
 import * as Immutable from "immutable";
+import {createMockStore} from "../testUtils";
 
 describe('(player) playerReducer', () => {
 
@@ -46,6 +48,41 @@ describe('(player) playerReducer', () => {
       expect(state).to.equal(previousState.setIn(["ownCharacters"],
         Immutable.fromJS([{id: 11, name: "ad"}])));
     });
+
+    const charId = "DEF";
+
+    it('Should request own characters list.', () => {
+      const charactersList = [
+        {
+          id: "ALA",
+          name: "John",
+        },
+        {
+          id: "REQ",
+          name: "Sally",
+        },
+      ];
+
+      const store = createMockStore({}, [charactersList]);
+
+      store.dispatch(requestOwnCharactersList());
+      store.socketCalledWith("player.get_characters_list");
+
+      const actions = store.getActions();
+      expect(actions).to.have.length(1);
+      expect(actions[0]).to.deep.equal({
+        type: UPDATE_OWN_CHARACTERS_LIST,
+        characterIdsList: charactersList,
+      });
+    });
+
+    it('Should request creation of a new character.', () => {
+      const store = createMockStore({}, []);
+      const newCharName = "NEW_CHAR_NAME";
+      store.dispatch(createNewCharacter(newCharName));
+      store.socketCalledWith("player.create_new_character", newCharName);
+      store.socketCalledWith("player.get_characters_list");
+    });
   });
 
   describe('achievements', () => {
@@ -65,6 +102,31 @@ describe('(player) playerReducer', () => {
       expect(state).to.equal(Immutable.fromJS({
         achievements: [{title: "abc", content: "be able to read abc"}]
       }));
+    });
+
+    const charId = "DEF";
+
+    it('Should request achievements list.', () => {
+      const achievementsList = [
+        {
+          name: "Potato eater",
+        },
+        {
+          name: "Cookie clicker",
+        },
+      ];
+
+      const store = createMockStore({}, [achievementsList]);
+
+      store.dispatch(requestAchievementsList());
+      store.socketCalledWith("player.get_achievements_list");
+
+      const actions = store.getActions();
+      expect(actions).to.have.length(1);
+      expect(actions[0]).to.deep.equal({
+        type: UPDATE_ACHIEVEMENTS_LIST,
+        achievementsList: achievementsList,
+      });
     });
   });
 });
