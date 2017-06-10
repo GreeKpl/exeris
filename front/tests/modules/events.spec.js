047e027f6e0bd2b025b15510a04f4dfb00377806
GreeK <grkalk@gmail.com>
2017-06-10 10:51:48 +0200

    [197] Test async methods in the 'events' module

--- front/tests/modules/events.spec.js
+++ front/tests/modules/events.spec.js
@@ -4,9 +4,10 @@ import {
   fromEventsState,
   updateEventsList,
   appendToEventsList,
-  decoratedEventsReducer
+  decoratedEventsReducer, UPDATE_EVENTS_LIST, requestAllEvents
 } from "../../src/modules/events";
 import * as Immutable from "immutable";
+import {createMockStore} from "../testUtils";
 
 describe('(events) eventsReducer', () => {
 
@@ -73,5 +74,28 @@ describe('(events) eventsReducer', () => {
     expect(getAllEvents(fromEventsState(state, "DEF"))).to.equal(Immutable.fromJS([
       {"id": 1, "text": "abc"},
     ]));
+  });
+
+  it('Should request all events for a character.', () => {
+    const charId = "DELELE";
+    const eventsList = [
+      {
+        id: 123,
+        text: "Event text",
+      },
+    ];
+
+    const store = createMockStore({}, [eventsList]);
+
+    store.dispatch(requestAllEvents(charId));
+    store.socketCalledWith("character.get_all_events", charId);
+
+    const actions = store.getActions();
+    expect(actions).to.have.length(1);
+    expect(actions[0]).to.deep.equal({
+      type: UPDATE_EVENTS_LIST,
+      eventsList: eventsList,
+      characterId: charId,
+    });
   });
 });
