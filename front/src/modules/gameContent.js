4c5f979c746fb49423b22a2b22dc50141e44f2f7
GreeK <grkalk@gmail.com>
2017-07-03 23:28:34 +0200

    [89] Add more action creators for gameContent module

--- front/src/modules/gameContent.js
+++ front/src/modules/gameContent.js
@@ -1,8 +1,10 @@
 import * as Immutable from "immutable";
+import {initialize} from "redux-form";
 
 export const UPDATE_LIST_OF_ENTITY_TYPES = "exeris-front/gameContent/UPDATE_LIST_OF_ENTITY_TYPES";
 export const SELECT_ENTITY_TYPE = "exeris-front/gameContent/SELECT_ENTITY_TYPE";
 export const CREATE_ENTITY_TYPE = "exeris-front/gameContent/CREATE_ENTITY_TYPE";
+export const UPDATE_ALL_PROPERTY_NAMES = "exeris-front/gameContent/UPDATE_ALL_PROPERTY_NAMES";
 
 
 export const CLASSES = {
@@ -36,10 +38,38 @@ export const requestEntityType = entityTypeName => {
 };
 
 export const openEntityTypeForm = entityType => {
-  return {
-    type: SELECT_ENTITY_TYPE,
-    entityType,
+  return dispatch => {
+    dispatch({
+      type: SELECT_ENTITY_TYPE,
+      entityType,
+    });
+
+    // force re-initialization of a form because it might have been already displayed for a different entity
+    dispatch(initialize("entityTypeManagement", entityType));
   };
+};
+
+export const requestAllPropertyNames = () => {
+  return (dispatch, getState, socket) => {
+    socket.request("admin.get_all_property_names", propertyNames => {
+      dispatch(updateAllPropertyNames(propertyNames));
+    });
+  };
+};
+
+export const updateAllPropertyNames = propertyNames => {
+  return {
+    type: UPDATE_ALL_PROPERTY_NAMES,
+    propertyNames,
+  };
+};
+
+export const requestUpdateOfEntityType = data => {
+  return (dispatch, getState, socket) => {
+    socket.request("admin.create_or_update_entity_type", data, () => {
+      dispatch(closeEntityTypeForm());
+    });
+  }
 };
 
 export const closeEntityTypeForm = () => {
@@ -49,22 +79,6 @@ export const closeEntityTypeForm = () => {
   }
 };
 
-export const requestUpdateEntityType = entityType => {
-  return (dispatch, getState, socket) => {
-    socket.request("admin.update_entity_type", entityType, () => {
-      dispatch(closeEntityTypeForm());
-    });
-  };
-};
-
-export const requestCreateNewEntityType = newEntityType => {
-  return (dispatch, getState, socket) => {
-    socket.request("admin.create_entity_type", newEntityType, () => {
-      dispatch(closeEntityTypeForm());
-    });
-  };
-};
-
 export const updateListOfEntityTypes = entityTypes => {
   return {
     type: UPDATE_LIST_OF_ENTITY_TYPES,
@@ -72,7 +86,10 @@ export const updateListOfEntityTypes = entityTypes => {
   };
 };
 
-export const gameContentReducer = (state = Immutable.fromJS({entityTypes: []}), action) => {
+export const gameContentReducer = (state = Immutable.fromJS({
+  entityTypes: [],
+  propertyNames: [],
+}), action) => {
   switch (action.type) {
     case UPDATE_LIST_OF_ENTITY_TYPES:
       return state.set("entityTypes", Immutable.fromJS(action.entityTypes));
@@ -82,14 +99,18 @@ export const gameContentReducer = (state = Immutable.fromJS({entityTypes: []}), 
     case CREATE_ENTITY_TYPE:
       return state.set("selectedEntityType", Immutable.fromJS(action.entityType))
         .set("newEntityType", false);
+    case UPDATE_ALL_PROPERTY_NAMES:
+      return state.set("propertyNames", Immutable.fromJS(action.propertyNames));
     default:
       return state;
   }
 };
 
-export const getAllEntityTypes = state => state.get("entityTypes", []);
+export const getAllEntityTypes = state => state.get("entityTypes", Immutable.List());
 
 export const getSelectedEntityType = state => state.get("selectedEntityType", null);
+
+export const getAllPropertyNames = state => state.get("propertyNames", Immutable.List());
 
 export const isNewEntityType = state => state.get("newEntityType", false);
 
